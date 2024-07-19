20fb455f3f77245dd3d277ba90fd8ea864876399
Ismaililica <ismaililica73@gmail.com>
2024-07-19 11:19:25 +0300

    Backend

--- eAppointmentServer.Infrastructure/DependencyInjection.cs
+++ eAppointmentServer.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using eAppointmentServer.Domain.Entities;
+﻿using eAppointmentServer.Application.Services;
+using eAppointmentServer.Domain.Entities;
 using eAppointmentServer.Domain.Repositories;
 using eAppointmentServer.Infrastructure.Context;
 using eAppointmentServer.Infrastructure.Repositories;
@@ -6,6 +7,7 @@ using GenericRepository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
 
 namespace eAppointmentServer.Infrastructure
 {
@@ -26,9 +28,19 @@ namespace eAppointmentServer.Infrastructure
                 action.Password.RequireNonAlphanumeric = false;
             });
             services.AddScoped<IAppointmentRepository, AppointmentRepository>();
-            services.AddScoped<IDoctorRepository, DoctorRepository>();
-            services.AddScoped<IPatientRepository, PatientRepository>();
-            services.AddScoped<IUnitOfWork>(srv =>srv.GetRequiredService<ApplicationDbContext>());
+            services.Scan(action
+                =>
+            {
+                action
+                .FromAssemblies(typeof(DependencyInjection).Assembly)
+                .AddClasses(publicOnly: false)
+                .UsingRegistrationStrategy(registrationStrategy: RegistrationStrategy.Skip)
+                .AsImplementedInterfaces()
+                .WithScopedLifetime();
+
+
+
+            });
             return services;
         }
     }
--- eAppointmentServer.Infrastructure/Services/JwtProvider.cs
+++ eAppointmentServer.Infrastructure/Services/JwtProvider.cs
@@ -1,6 +1,9 @@
 ﻿using eAppointmentServer.Application.Services;
 using eAppointmentServer.Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace eAppointmentServer.Infrastructure.Services
 {
@@ -8,12 +11,23 @@ namespace eAppointmentServer.Infrastructure.Services
     {
         public string CreateToken(AppUser user)
         {
+            List<Claim> claims = new()
+            {
+                new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
+                new Claim(ClaimTypes.Name,user.FullName.ToString()),
+                new Claim(ClaimTypes.Email,user.Email ?? string.Empty),
+                new Claim("UserName",user.UserName?? string.Empty)
+            };
+            DateTime expires = DateTime.Now.AddDays(1);
+            SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes(string.Join("-", Guid.NewGuid())));
+            SigningCredentials signingCredentials = new(securityKey,SecurityAlgorithms.HmacSha512);
+
             JwtSecurityToken securityToken = new(issuer: "Ismail Ilica",
                 audience: "eAppointment",
-                claims: null,
-                notBefore: null,
-                expires: null,
-                signingCredentials: null);
+                claims: claims,
+                notBefore: DateTime.Now,
+                expires: expires,
+                signingCredentials: signingCredentials);
 
             JwtSecurityTokenHandler handler = new();
 
